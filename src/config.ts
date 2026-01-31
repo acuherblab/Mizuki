43451fb36bbd946c16feef4bc82bc0db9a766d08
acuherb <209729051+acuherb@users.noreply.github.com>
2026-01-31 20:51:09 +0800

    update

--- src/config.ts
+++ src/config.ts
@@ -354,8 +354,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "まつざか ゆき",
-	bio: "世界は大きい、君は行かなければならない",
+	name: "观颐 ",
+	bio: "世观以明理，颐以养正",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
 		speed: 80, // 打字速度（毫秒）
